d0e998ab7c9c9fdf88a5e9928bdc002f0dac637d
gurupanguji <gurupanguji@gmail.com>
2025-01-12 13:57:04 -0800

    Bringing the prompt character back along with execution time and exit code

--- .config/spaceship.zsh
+++ .config/spaceship.zsh
@@ -5,6 +5,10 @@ SPACESHIP_PROMPT_ORDER=(
   dir
   time
   git
+  exec_time
+  exit_code
+  line_sep
+  char
 )
 # Display time
 SPACESHIP_TIME_SHOW=true
@@ -33,9 +37,10 @@ SPACESHIP_DIR_TRUNC_REPO=false
 SPACESHIP_GIT_STATUS_SHOW=true
 SPACESHIP_GIT_STATUS_ASYNC=true
 
+# show exit code
+SPACESHIP_EXIT_CODE_SHOW=true
+
 # new line
 SPACESHIP_PROMPT_ADD_NEWLINE=true
 SPACESHIP_PROMPT_SEPARATE_LINE=true
-
-# show exit code
-SPACESHIP_EXIT_CODE_SHOW=true
+SPACESHIP_CHAR_SYMBOL_ROOT="#"
